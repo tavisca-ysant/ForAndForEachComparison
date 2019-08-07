37ac935c47f5b4163ec57d5540bfd8c48ca23c99
tavisca-ysant <ysant@tavisca.in>
2019-08-07 21:57:06 +0530

    First commit

---
+++ ForAndForEachComparison/Comparison.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace ForAndForEachComparison
+{
+    [MemoryDiagnoser]
+    public class Comparison
+    {
+         List<int> _numbers = new List<int>();
+       
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                _numbers.Add(i);
+            }
+        }
+
+        [Benchmark]
+
+        public void ForLoop()
+        {
+            List<int> numbers = new List<int>();
+            for(int i = 0; i < 1000; i++)
+            {
+                numbers.Add(_numbers[i]);
+            }
+        }
+
+        [Benchmark]
+
+        public void ForEachLoop()
+        {
+            List<int> numbers = new List<int>();
+            foreach (var item in _numbers)
+            {
+                numbers.Add(item);
+            }
+        }
+    }
+}
